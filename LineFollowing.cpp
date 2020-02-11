2c4cbbb47636f1672e9a293a7960d109c39b263b
Azael-Ruh <42300430+Azael-Ruh@users.noreply.github.com>
2020-02-11 21:26:19 +0100

    Update LineFollowing.cpp

--- LineFollowing.cpp
+++ LineFollowing.cpp
@@ -2,9 +2,16 @@
 #include "Constants.h"
 
 void setup(){
-
+  //Inicialización de pines (I/O)
+  
+  //Configuración timers
+  
+  //Estado inicial
 }
 
 void loop(){
-
+  //Switch de actualización de estados  
+  switch(estado){
+  }
+  
 }
